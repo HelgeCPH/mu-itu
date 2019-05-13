7a4eb4867ec36fbe266031651c97d5108f0f730a
Helge <ropf@itu.dk>
2019-05-13 11:07:30 +0200

    Cleaned up build script

--- build_python.sh
+++ build_python.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo chown vagrant "$HOME"/host/*
-HOST="$/Users/vagrant/host"
+#sudo chown vagrant "$HOME"/host/*
+HOST="/Users/vagrant/host"
 # On the MacOS VM, brew and OpenSSL are already installed
 
 # For some reason the build for 3.7.3 cannot find the OpenSSL stuff...
@@ -30,13 +30,8 @@ wget https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz
 tar -zxvf Python-$VERSION.tgz &> /dev/null
 
 cd Python-$VERSION || exit
+
 echo "Configuring Python..."
-# ./configure MACOSX_DEPLOYMENT_TARGET=10.14 CPPFLAGS="-I$OPENSSL_ROOT/include" LDFLAGS="-L$OPENSSL_ROOT/lib" CFLAGS="-I$(xcrun --show-sdk-path)/usr/include" --prefix="$CURRENT_DIR/python"
-
-#./configure CPPFLAGS="-I/usr/local/opt/openssl/include -I/usr/local/opt/zlib/include -L/usr/local/opt/tcl-tk/lib" LDFLAGS="-L/usr/local/opt/openssl/lib -L/usr/local/opt/zlib/lib -L/usr/local/opt/tcl-tk/lib" --prefix="$CURRENT_DIR/python"
-
-#./configure CPPFLAGS="-I/usr/local/opt/openssl/include" LDFLAGS="-L/usr/local/opt/openssl/lib" --prefix="$CURRENT_DIR/python" --enable-optimizations
-
 ./configure CPPFLAGS="-I/usr/local/opt/openssl/include -I/usr/local/opt/zlib/include -L/usr/local/opt/tcl-tk/lib" LDFLAGS="-L/usr/local/opt/openssl/lib -L/usr/local/opt/zlib/lib -L/usr/local/opt/tcl-tk/lib" --prefix="$BUILD_OUT" --enable-optimizations > "$CURRENT_DIR"/configure.log 2>&1
 
 echo "Building Python..."
@@ -55,6 +50,7 @@ echo "Turning the Python distribution into a Mu Editor App..."
 echo "Installing Mu Editor..."
 "$BUILD_OUT"/bin/pip3.6 install mu-editor
 
+echo "Copying our wrapper functions..."
 cp "$HOST"/mu_env.sh "$BUILD_OUT"/bin
 #TODO: Replace shebang of pip3.6 of mu-editor in bin
 # I will just do that for all scripts in there...
@@ -68,6 +64,7 @@ cp "$HOST"/mu_env.sh "$BUILD_OUT"/bin
 #     fi
 # done
 
+echo "Replacing shebangs of scripts in bin..."
 # Multiline strings in Bash come from here: https://stackoverflow.com/a/23930212
 read -r -d '' _inner_loop <<"EOF"
 for SCRPT in $1
@@ -79,18 +76,9 @@ do
     fi
 done
 EOF
-
 # -exec comes from https://unix.stackexchange.com/a/193873
 find "$BUILD_OUT"/bin -type f -exec bash -c "$_inner_loop" none {}  \;
 
-
-
-
-
-
-#for SCRPT in `find $BUILD_OUT/bin -type f`; do sed -i '' "s/Users\/vagrant\/python\/bin\/python3\.6/Applications\/mu-editor\.app\/Contents\/Resources\/python\/bin\/python3\.6/" $SCRPT; done
-
-# for SCRPT in `find $BUILD_OUT/bin -type f`; do echo $SCRPT; if file -b $SCRPT | grep 'ASCII' &>/dev/null; then head -1 $SCRPT; fi; done
 echo "Hacking together a MacOS application..."
 mkdir -p "$HOME"/mu-editor.app/Contents/Resources
 mkdir -p "$HOME"/mu-editor.app/Contents/MacOS
@@ -102,14 +90,13 @@ cp -r "$BUILD_OUT" "$HOME"/mu-editor.app/Contents/Resources
 
 echo '#!/bin/sh' > "$HOME"/mu-editor.app/Contents/MacOS/mu-editor
 echo "" >> "$HOME"/mu-editor.app/Contents/MacOS/mu-editor
-# echo "echo OIOIOI" >> $HOME/mu-editor.app/Contents/MacOS/mu-editor
 echo "/Applications/mu-editor.app/Contents/Resources/python/bin/mu-editor" >> "$HOME"/mu-editor.app/Contents/MacOS/mu-editor
 chmod u+x "$HOME"/mu-editor.app/Contents/MacOS/mu-editor
 
 echo "That should be it..."
 
 echo "Building a DMG"
-#TODO!
+# TODO!
 # Have a look at this one: https://github.com/andreyvit/create-dmg
 
 echo "Compressing DMG"
@@ -125,14 +112,3 @@ mv mu-editor.tar.bz2 "$HOME"/host
 mv "$HOME"/mu-editor.app /Applications
 
 echo "source /Applications/mu-editor.app/Contents/Resources/python/bin/mu_env.sh" >> ~/.bash_profile
-
-# curl --upload-file ./upload/python3-full.tar.gz https://transfer.sh/python3-full.tar.gz | tee -a output_urls.txt && echo "" >> output_urls.txt
-
-
-
-#   # Reduce stand-alone Python and upload it
-#   - cd "$PROJECT_DIR"
-#   - python process_python_build.py "$PROJECT_DIR/python"
-#   - echo "Python 3.6.5" >> ./python/version.txt
-#   - tar czf upload/python3-reduced.tar.gz python/
-#   - curl --upload-file ./upload/python3-reduced.tar.gz https://transfer.sh/python3-reduced.tar.gz | tee -a output_urls.txt && echo "" >> output_urls.txt
